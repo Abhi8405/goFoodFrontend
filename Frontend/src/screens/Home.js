297c469314a6ee5f96ea512b0b97803a580f5212
Abhishek <abhishekk10112001@gmail.com>
2024-07-24 01:24:32 +0530

    First Commit

---
+++ Frontend/src/screens/Home.js
@@ -0,0 +1,107 @@
+
+import React, { useEffect, useState } from 'react'
+
+import Card from '../components/Card'
+// import Carousel from '../components/Carousel'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const BASE_URL= process.env.REACT_APP_BASE_URL;
+export default function Home() {
+  const [foodCat, setFoodCat] = useState([])
+  const [foodItems, setFoodItems] = useState([])
+  const [search, setSearch] = useState('')
+  const loadFoodItems = async () => {
+    let response = await fetch(`${BASE_URL}/api/auth/foodData`, {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    });
+    response = await response.json()
+    console.log(response)
+    setFoodItems(response[0])
+    setFoodCat(response[1])
+  }
+
+  useEffect(() => {
+    loadFoodItems()
+  }, [])
+
+  return (
+    <div >
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
+
+          <div className="carousel-inner " id='carousel'>
+            <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+              <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
+              </div>
+            </div>
+            <div className="carousel-item active" >
+              <img src="https://img.freepik.com/free-photo/grilled-cheeseburger-with-tomato-sesame-bun-generative-ai_188544-12302.jpg?w=740&t=st=1720852445~exp=1720853045~hmac=39d95ca313d8505045fdacc9298c500db3cbd6f71a4f90f7a6ba2210f7572d92" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://w7.pngwing.com/pngs/736/269/png-transparent-food-background-food-fruit-gray-thumbnail.png" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className='container'> {/* boootstrap is mobile first */}
+        {
+          foodCat !== []
+            ? foodCat.map((data) => {
+              return (
+                // justify-content-center
+                <div className='row mb-3'>
+                  <div key={data.id} className='fs-3 m-3'>
+                    {data.CategoryName}
+                  </div>
+                  <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
+                  {foodItems !== [] ? foodItems.filter(
+                    (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
+                    .map(filterItems => {
+                      return (
+                        <div key={filterItems.id} className='col-12 col-md-6 col-lg-3'>
+                          {console.log(filterItems.url)}
+                          <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
+                        </div>
+                      )
+                    }) : <div> No Such Data </div>}
+                </div>
+              )
+            })
+            : ""}
+      </div>
+      <Footer />
+    </div>
+
+
+
+
+
+
+
+
+
+  )
+}
